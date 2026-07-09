d91ae115720b9bd3ce939dfc3d79e3bb369faddd
zorrokurro <layjack102389@gmail.com>
2026-07-10 02:59:20 +0800

    fix: basePath + 清除 out/

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
+  basePath: "/strada-verso-game",
   images: {
     unoptimized: true,
   },
